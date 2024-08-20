d62df3d55d1bb99ecfe93e02198e0100b11895ed
Inbeom Kim <dlsqja970210@naver.com>
2024-08-20 18:32:59 +0900

    🎉 프로젝트 시작

---
+++ src/main/java/paengbeom/syono/SyonoApplication.java
@@ -0,0 +1,13 @@
+package paengbeom.syono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyonoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/paengbeom/syono/SyonoApplicationTests.java
@@ -0,0 +1,13 @@
+package paengbeom.syono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
